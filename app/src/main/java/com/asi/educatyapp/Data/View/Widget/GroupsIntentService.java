ec1155fe641f4f2bbf827abc77b39c8d7c27b642
ebnrdwan <eng.abdoehab@gmail.com>
2017-07-20 07:18:09 +0200

    cleaning

---
+++ app/src/main/java/com/asi/educatyapp/Data/View/Widget/GroupsIntentService.java
@@ -0,0 +1,75 @@
+package com.asi.educatyapp.Data.View.Widget;
+
+import android.app.IntentService;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+import com.asi.educatyapp.Data.Data.Models.GroupsModel;
+import com.asi.educatyapp.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Abdulrhman on 18/07/2017.
+ */
+
+public class GroupsIntentService extends IntentService {
+    public static final String ACTION_GROUPS = " com.asi.educatyapp.Data.View.Widget.GroupsIntentService.groups_action";
+public ArrayList<GroupsModel> groupList =new ArrayList<>();
+Context context;
+
+    public GroupsIntentService() {
+        //empty constructor required by the AndroidManifest
+        super(GroupsIntentService.class.getName());
+    }
+    public static void startActionGroupsService(Context context) {
+        Intent intent = new Intent(context, GroupsIntentService.class);
+        intent.setAction(ACTION_GROUPS);
+        context.startService(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        if (intent != null) {
+            String action = intent.getAction();
+if (ACTION_GROUPS.equals(action))
+               handleUpdateWidget(getApplicationContext());
+
+
+
+        }
+    }
+
+    private ArrayList<GroupsModel> handleUpdateWidget(final Context context){
+
+
+//        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child(FirebaseUtil.groupsObject);
+//        FirebaseListAdapter adapter = new FirebaseListAdapter<GroupsModel>(GroupsIntentService.this,GroupsModel.class, R.layout.groupsitem,databaseReference) {
+//
+//
+//            @Override
+//            protected void populateView(View v, GroupsModel model, int position) {
+//             if (groupList.size()<20){
+//                 groupList.add(model);
+//             }
+//            }
+//        };
+
+        GroupsModel model = new GroupsModel("id","tid","name","https://firebasestorage.googleapis.com/v0/b/educaty-9304b.appspot.com/o/Profile_photo%2Fstudentsample.jpg?alt=media&token=2a970b70-1b7f-4b27-b4b7-9805cc8f348e");
+        groupList.add(model);
+
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(this, GroupsWidgetProvider.class));
+        //Trigger data update to handle the GridView widgets and force a data refresh
+        appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widgetGrid);
+        //Now update all widgets
+
+
+            GroupsWidgetProvider.updatewholeAppWidget(this,appWidgetManager,appWidgetIds);
+
+        return groupList;
+    }
+}
--- app/src/main/java/com/asi/educatyapp/Data/View/Widget/GroupsWidgetProvider.java
+++ app/src/main/java/com/asi/educatyapp/Data/View/Widget/GroupsWidgetProvider.java
@@ -5,7 +5,6 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.widget.RemoteViews;
 
 import com.asi.educatyapp.Data.View.Activities.Groups;
@@ -20,47 +19,28 @@ public class GroupsWidgetProvider extends AppWidgetProvider {
         super.onReceive(context, intent);
     }
 
-    static void updateAppWidget(final Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+    public static void updatewholeAppWidget(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+            GroupsWidgetProvider.updatewholeAppWidget(context, appWidgetManager, appWidgetIds);
+        }
+    }
 
+    static void updateAppWidget(final Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
         // Construct the RemoteViews object
 
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.groups_widget);
-        Intent intent = new Intent(context, Groups.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-        views.setOnClickPendingIntent(R.id.image_viewwidget, pendingIntent);
-        // Instruct the widget manager to update the widget
-        Intent intent1 = new Intent(context,WidgetRemoteViewFactory.class);
-        PendingIntent pendingIntent1 = PendingIntent.getActivity(context,0,intent1,0);
-        views.setOnClickPendingIntent(R.id.sync_widget,pendingIntent1);
-        views.setRemoteAdapter(R.id.widgetGrid,intent1);
-        views = getGardenGridRemoteView(context);
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-        Intent GroupServiceIntent = new Intent(context, GroupsService.class);
-        GroupServiceIntent.setAction(GroupsService.ACTION_GROUPS);
-        PendingIntent servicePendingIntent = PendingIntent.getService(
-                context,
-                0,
-                GroupServiceIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
-        views.setRemoteAdapter(R.id.sync_widget, GroupServiceIntent);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-            views.setRemoteAdapter(R.id.widgetGrid,
-                    new Intent(context, RemoteWidgetServiceAdapter.class).putExtra("id",appWidgetId));
-        } else {
-            views.setRemoteAdapter(0, R.id.widgetGrid,
-                    new Intent(context, RemoteWidgetServiceAdapter.class).putExtra("id",appWidgetId));
-        }
+        RemoteViews remoteViews = getGridItemsRemoteView(context);
+        appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+
+
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
-        Intent serIntent = new Intent(context,RemoteWidgetServiceAdapter.class);
-        context.startService(serIntent);
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
-
-        }
+//        Intent serIntent = new Intent(context, RemoteWidgetServiceAdapter.class);
+//        context.startService(serIntent);
+        GroupsIntentService.startActionGroupsService(context);
     }
 
     @Override
@@ -73,10 +53,17 @@ public class GroupsWidgetProvider extends AppWidgetProvider {
         // Enter relevant functionality for when the last widget is disabled
     }
 
-    private static RemoteViews getGardenGridRemoteView(Context context) {
+    private static RemoteViews getGridItemsRemoteView(Context context) {
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.groups_widget);
+        Intent groupIntent = new Intent(context, Groups.class);
+        PendingIntent groupPendingIntent = PendingIntent.getActivity(context, 0, groupIntent, 0);
+        views.setOnClickPendingIntent(R.id.image_viewwidget, groupPendingIntent);
+        // Instruct the widget manager to update the widget
+        Intent syncIntent = new Intent(context, GroupsIntentService.class);
+        PendingIntent syncPendingIntent = PendingIntent.getActivity(context, 0, syncIntent, 0);
+        views.setOnClickPendingIntent(R.id.sync_widget, syncPendingIntent);
         // Set the GridWidgetService intent to act as the adapter for the GridView
-        Intent intent = new Intent(context, WidgetRemoteViewFactory.class);
+        Intent intent = new Intent(context, RemoteWidgetServiceAdapter.class);
         views.setRemoteAdapter(R.id.widgetGrid, intent);
         // Set the PlantDetailActivity intent to launch when clicked
         Intent appIntent = new Intent(context, Groups.class);
